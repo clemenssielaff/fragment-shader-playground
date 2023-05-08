40484e4a73651b100cc4bc315764cd268bd86881
Clemens Sielaff <git@clemens-sielaff.com>
2023-05-08 17:57:55 +0200

    Created good-looking first PBR render

--- 23.05.09/pbr/scene.js
+++ 23.05.09/pbr/scene.js
@@ -300,23 +300,31 @@ async function main() {
   mat4.perspective(projectionMatrix,
     0.7853981633974483, // 45deg field of view in radians
     aspectRatio,        // aspect ratio
-    0.1,                // near clipping plane
-    100,                // far clipping plane
+    10,                 // near clipping plane
+    40,                 // far clipping plane
   );
   
   const lightPositions = [
-    vec3.fromValues(-10,  10, 10),
-    vec3.fromValues( 10,  10, 10),
-    vec3.fromValues(-10, -10, 10),
-    vec3.fromValues( 10, -10, 10),
+    -10,  10, 10,
+     10,  10, 10,
+    -10, -10, 10,
+     10, -10, 10,
   ]
   
   const lightColors = [
-    vec3.fromValues(300, 300, 300),
-    vec3.fromValues(300, 300, 300),
-    vec3.fromValues(300, 300, 300),
-    vec3.fromValues(300, 300, 300),
+    300, 300, 300,
+    300, 300, 300,
+    300, 300, 300,
+    300, 300, 300,
   ]
+
+  let cameraDistance = 25;
+  const worldCenter = vec3.fromValues(0, 0, 0);
+
+  const initialCameraPosition = vec3.fromValues(0, 0, cameraDistance);
+  vec3.rotateY(initialCameraPosition, initialCameraPosition, worldCenter, 0);
+  const initialViewMatrix = mat4.create();
+  mat4.lookAt(initialViewMatrix, initialCameraPosition, worldCenter, vec3.fromValues(0, 1, 0));
 
   // Create the shaders.
   const pbrShader = await createShader(gl,
@@ -333,7 +341,7 @@ async function main() {
       },
      "uViewMatrix": {
           type: "mat4",
-          value: mat4.create()
+          value: initialViewMatrix
       },
      "uModelMatrix": {
           type: "mat4",
@@ -369,15 +377,90 @@ async function main() {
       },
       "uCamPos": {
           type: "vec3",
-          value: vec3.create()
+          value: initialCameraPosition
       },
     });
+
+
+  // Create the entities.
+  const sphereEntity = createSphere(gl, 
+    "Sphere Entity",
+    pbrShader,
+    {
+      normalAttribute: "aNormal",
+      latitudeBands: 64,
+      longitudeBands: 64,
+    }
+  );
 
   // Prepare the OpenGL state machine
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Set clear color to black, fully opaque
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing.
+
+  const nRows = 6;
+  const nCols = 6;
+  const spacing = 2.5;
+
+  // The render loop.
+  function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Render a grid of spheres.
+    for(let row = 0; row <= nRows; row++) {
+      // Update the metallic.
+      sphereEntity.uniform.uMetallic = row / nRows;
+
+      for(let col = 0; col <= nCols; col++) {
+        // Update the roughness.
+        sphereEntity.uniform.uRoughness = clamp(col / nCols, .05, 1);
+
+        // Update the model matrix.
+        const modelMatrix = mat4.create();
+        mat4.translate(modelMatrix, modelMatrix, [
+          (col - nCols / 2) * spacing, 
+          (row - nRows / 2) * spacing, 
+          0
+        ]);
+        sphereEntity.uniform.uModelMatrix = modelMatrix;
+
+        // Update the normal matrix.
+        const normalMatrix = mat3.create();
+        mat3.normalFromMat4(normalMatrix, modelMatrix);
+        sphereEntity.uniform.uNormalMatrix = normalMatrix;
+    
+        sphereEntity.render();
+      }
+    }
+  }
+
+  function handleMouseMove(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const rect = event.target.getBoundingClientRect();
+    const normalizedX = (x - rect.left) / rect.width;
+    const normalizedY = (y - rect.top) / rect.height;
+    const clipX = (normalizedX * 2 - 1) * Math.PI;
+    const clipY = (normalizedY * 2 - 1);
+    
+    const cameraPosition = vec3.fromValues(0, 0, cameraDistance);
+    vec3.rotateX(cameraPosition, cameraPosition, worldCenter, clipY);
+    vec3.rotateY(cameraPosition, cameraPosition, worldCenter, clipX);
+    
+    const viewMatrix = mat4.create();
+    mat4.lookAt(viewMatrix, cameraPosition, worldCenter, vec3.fromValues(0, 1, 0));
+
+    sphereEntity.uniform.uCamPos = cameraPosition;
+    sphereEntity.uniform.uViewMatrix = viewMatrix;
+
+    requestAnimationFrame(render);
+  }
+  document.addEventListener('mousemove', handleMouseMove);
+
+  // Render the first frame.
+  requestAnimationFrame(render);
 }
-main_test();
+
+main();
 
 
 // =============================================================================
@@ -1007,7 +1090,7 @@ async function createShader(gl, name, vertexPath, fragmentPath, attributes, unif
     const uniformObject = {
       type: uniformInfo.type,
       location,
-      _value: uniformInfo.value,
+      _value: null,
       set value(newValue) {
         if(newValue === this._value) { return; }
         this._value = newValue;
@@ -1020,6 +1103,9 @@ async function createShader(gl, name, vertexPath, fragmentPath, attributes, unif
         return this._value;
       }
     };
+
+    // Set the initial value.
+    uniformObject.value = uniformInfo.value;
 
     // Textures are a special case.
     // We want to store the texture as the uniform value, but we also need to 
@@ -1292,6 +1378,12 @@ function repeat(pattern, times)
 function isPowerOf2(value) 
 {
   return (value & (value - 1)) === 0;
+}
+
+/// Clamps the given value between the given min and max values.
+function clamp(value, min, max)
+{
+  return Math.min(Math.max(value, min), max);
 }
 
 /// Interleave the given arrays, taking a number of elements (quantity) from each array in turn.
