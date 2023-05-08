def189f7eede175057323f036d7e98138862e8b1
Clemens Sielaff <git@clemens-sielaff.com>
2023-05-08 23:49:58 +0200

    Separared engine and scenes

---
+++ 23.05.09/pbr/scenes/engine_test.js
@@ -0,0 +1,144 @@
+// Transated to Javascript from 
+//  https://github.com/JoeyDeVries/LearnOpenGL/blob/master/src/6.pbr/2.2.2.ibl_specular_textured/ibl_specular_textured.cpp
+
+
+const { vec3, mat3, mat4 } = glMatrix;
+
+import * as engine from "../engine.js";
+
+async function main() {
+  // Get the WebGL context from the canvas element in the DOM.
+  const gl = document.querySelector("#canvas").getContext('webgl2');
+  if (!gl) {
+      console.log('WebGL unavailable');
+  } else {
+      console.log('WebGL is good to go');
+  }
+
+  // Create the textures.
+  const texture = await engine.createTexture(gl, "brick_wall_001_diffuse_1k.png");
+
+  // Create the framebuffers.
+  const offscreenFramebuffer = engine.createFramebuffer(gl, "Offscreen Framebuffer");
+  const greyscaleFramebuffer = engine.createFramebuffer(gl, "Greyscale Framebuffer");
+
+  // Create the shaders.
+  const flatShader = await engine.createShader(gl,
+    "Flat Shader",                  // name
+    "flat.vert",                    // vertex shader source file
+    "flat.frag",                    // fragment shader source file
+    [                               // attributes
+      "aPosition",
+      "aTexCoord"
+    ], {                            // uniforms
+      "uModelViewMatrix": {
+          type: "mat4",
+          value: mat4.create()
+      }, 
+      "uProjectionMatrix": {
+          type: "mat4",
+          value: mat4.create()
+      }, 
+      "uTexture": {
+          type: "sampler2D",
+          value: texture
+      }, 
+    });
+  const greyscaleShader = await engine.createShader(gl,
+    "Greyscale Shader",             // name
+    "quad.vert",                    // vertex shader source file
+    "greyscale.frag",               // fragment shader source file
+    [                               // attributes
+      "aPosition",
+      "aTexCoord"
+    ], {                            // uniforms
+      "uTexture": {
+          type: "sampler2D",
+          value: offscreenFramebuffer.color,
+      }
+    });
+  const quadShader = await engine.createShader(gl,
+    "Quad Shader",                  // name
+    "quad.vert",                    // vertex shader source file
+    "quad.frag",                    // fragment shader source file
+    [                               // attributes
+      "aPosition",
+      "aTexCoord"
+    ], {                            // uniforms
+      "uTexture": {
+          type: "sampler2D",
+          value: greyscaleFramebuffer.color,
+      }
+    });
+
+  // Create the entities.
+  const quadEntity = engine.createFullscreenQuad(gl,
+    "Quad",
+    quadShader,
+  );
+  const greyscaleEntity = engine.createEntity(gl, // copy the quad entity geometry
+    "Greyscale Quad",
+    quadEntity.vertexBuffers,
+    quadEntity.indexBuffer,
+    greyscaleShader,
+  );
+  const geoEntity = engine.createBox(gl, 
+    "Geo Entity", 
+    flatShader, 
+    {
+      texCoordAttribute: "aTexCoord",
+    }
+  );
+
+  // Prepare the OpenGL state machine
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black, fully opaque
+  gl.enable(gl.DEPTH_TEST);   // Enable depth testing.
+  gl.depthFunc(gl.LEQUAL);    // Set depth function to less than AND equal for skybox depth trick.
+  // Enable backface culling.
+  gl.enable(gl.CULL_FACE);
+  gl.cullFace(gl.BACK);
+
+  // Create a perspective projection matrix.
+  const projectionMatrix = mat4.create();
+  mat4.perspective(projectionMatrix,
+    0.7853981633974483,       // 45deg field of view in radians
+    (gl.canvas.clientWidth /  // aspect ratio
+      gl.canvas.clientHeight),
+    0.1,                      // near clipping plane
+    100);                     // far clipping plane
+  
+  // Define static uniforms.
+  geoEntity.uniform.uProjectionMatrix = projectionMatrix;
+  geoEntity.uniform.uTexture = texture;
+
+  function updateGeo() {
+    // Position the model in front of the camera.
+    const modelViewMatrix = mat4.create();
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0, 0, -5]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, 0.6, [1, 0, 0]);
+
+    geoEntity.uniform.uModelViewMatrix = modelViewMatrix;
+  }
+
+  function render(now) {
+    updateGeo();
+
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    offscreenFramebuffer.use();
+    geoEntity.render();
+    offscreenFramebuffer.unuse();
+
+    greyscaleFramebuffer.use();
+    greyscaleEntity.render();
+    greyscaleFramebuffer.unuse();
+
+    quadEntity.render();
+
+    // Call render() again next frame.
+    // requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+main();
---
+++ 23.05.09/pbr/scenes/pbr_pointlights.js
@@ -0,0 +1,183 @@
+// Transated to Javascript from 
+//  https://learnopengl.com/code_viewer_gh.php?code=src/6.pbr/1.1.lighting/lighting.cpp
+
+const { vec3, mat3, mat4 } = glMatrix;
+
+import * as engine from "../engine.js";
+
+async function main() {
+  // Get the WebGL context from the canvas element in the DOM.
+  const gl = document.querySelector("#canvas").getContext('webgl2');
+  if (!gl) {
+      console.log('WebGL unavailable');
+  } else {
+      console.log('WebGL is good to go');
+  }
+
+  // Create uniform constants
+  const aspectRatio = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const projectionMatrix = mat4.create();
+  mat4.perspective(projectionMatrix,
+    0.7853981633974483, // 45deg field of view in radians
+    aspectRatio,        // aspect ratio
+    10,                 // near clipping plane
+    40,                 // far clipping plane
+  );
+  
+  const lightPositions = [
+    -10,  10, 10,
+     10,  10, 10,
+    -10, -10, 10,
+     10, -10, 10,
+  ]
+  
+  const lightColors = [
+    300, 300, 300,
+    300, 300, 300,
+    300, 300, 300,
+    300, 300, 300,
+  ]
+
+  let cameraDistance = 20;
+  const worldCenter = vec3.fromValues(0, 0, 0);
+
+  const initialCameraPosition = vec3.fromValues(0, 0, cameraDistance);
+  vec3.rotateY(initialCameraPosition, initialCameraPosition, worldCenter, 0);
+  const initialViewMatrix = mat4.create();
+  mat4.lookAt(initialViewMatrix, initialCameraPosition, worldCenter, vec3.fromValues(0, 1, 0));
+
+  // Create the shaders.
+  const pbrShader = await engine.createShader(gl,
+    "PBR Shader",                   // name
+    "pbr1.vert",                    // vertex shader source file
+    "pbr1.frag",                    // fragment shader source file
+    [                               // attributes
+      "aPosition",
+      "aNormal",
+    ], {                            // uniforms
+      "uProjectionMatrix": {
+          type: "mat4",
+          value: projectionMatrix
+      },
+     "uViewMatrix": {
+          type: "mat4",
+          value: initialViewMatrix
+      },
+     "uModelMatrix": {
+          type: "mat4",
+          value: mat4.create()
+      },
+     "uNormalMatrix": {
+          type: "mat3",
+          value: mat3.create()
+      },
+      "uAlbedo": {
+          type: "vec3",
+          value: vec3.fromValues(.5, 0, 0)
+      },
+      "uMetallic": {
+          type: "float",
+          value: 0.0
+      },
+      "uRoughness": {
+          type: "float",
+          value: 0.0
+      },
+      "uAmbientOcclusion": {
+          type: "float",
+          value: 1.0
+      },
+      "uLightPositions": {
+          type: "vec3",
+          value: lightPositions
+      },
+      "uLightColors": {
+          type: "vec3",
+          value: lightColors
+      },
+      "uCamPos": {
+          type: "vec3",
+          value: initialCameraPosition
+      },
+    });
+
+
+  // Create the entities.
+  const sphereEntity = engine.createSphere(gl, 
+    "Sphere Entity",
+    pbrShader,
+    {
+      normalAttribute: "aNormal",
+      latitudeBands: 64,
+      longitudeBands: 64,
+    }
+  );
+
+  // Prepare the OpenGL state machine
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Set clear color to black, fully opaque
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing.
+
+  const nRows = 4;
+  const nCols = 4;
+  const spacing = 2.5;
+
+  // The render loop.
+  function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Render a grid of spheres.
+    for(let row = 0; row <= nRows; row++) {
+      // Update the metallic.
+      sphereEntity.uniform.uMetallic = row / nRows;
+
+      for(let col = 0; col <= nCols; col++) {
+        // Update the roughness.
+        sphereEntity.uniform.uRoughness = Math.min(Math.max(col / nCols, .05), 1);
+
+        // Update the model matrix.
+        const modelMatrix = mat4.create();
+        mat4.translate(modelMatrix, modelMatrix, [
+          (col - nCols / 2) * spacing, 
+          (row - nRows / 2) * spacing, 
+          0
+        ]);
+        sphereEntity.uniform.uModelMatrix = modelMatrix;
+
+        // Update the normal matrix.
+        const normalMatrix = mat3.create();
+        mat3.normalFromMat4(normalMatrix, modelMatrix);
+        sphereEntity.uniform.uNormalMatrix = normalMatrix;
+    
+        sphereEntity.render();
+      }
+    }
+  }
+
+  function handleMouseMove(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const rect = event.target.getBoundingClientRect();
+    const normalizedX = (x - rect.left) / rect.width;
+    const normalizedY = (y - rect.top) / rect.height;
+    const clipX = (normalizedX * 2 - 1) * Math.PI;
+    const clipY = (normalizedY * 2 - 1) * Math.PI * .4;
+    
+    const cameraPosition = vec3.fromValues(0, 0, cameraDistance);
+    vec3.rotateX(cameraPosition, cameraPosition, worldCenter, clipY);
+    vec3.rotateY(cameraPosition, cameraPosition, worldCenter, clipX);
+    
+    const viewMatrix = mat4.create();
+    mat4.lookAt(viewMatrix, cameraPosition, worldCenter, vec3.fromValues(0, 1, 0));
+
+    sphereEntity.uniform.uCamPos = cameraPosition;
+    sphereEntity.uniform.uViewMatrix = viewMatrix;
+
+    requestAnimationFrame(render);
+  }
+  document.addEventListener('mousemove', handleMouseMove);
+
+  // Render the first frame.
+  requestAnimationFrame(render);
+}
+
+main();
